585c306c73a4eedd04d13fe09aa6b446e88e3a23
Tanner Galloway <tannergalloway15@gmail.com>
2019-02-09 14:07:33 -0700

    Compleated user request and bonus

--- liri.js
+++ liri.js
@@ -214,3 +214,15 @@ function song()
       } 
     });
 }
+
+function log(text)
+{
+    // append text to file given 
+    fs.appendFile("log.txt", text + "\n", function(error)
+    {
+        if(error)
+        {
+            console.log(error);
+        }
+    });
+}
